b46769dbf0dcac3c4c989a23bae58950285770b3
Wajiddat18c <waji0003@stud.kea.dk>
2020-02-07 10:47:36 +0100

    Added the first exercises

---
+++ 2/1._Intro_Files/firstFile.js
@@ -0,0 +1,16 @@
+// + make it concatenate
+console.log("Hello" + "World");
+// , make is not concatenate
+console.log("Hello", "World");
+
+
+var name = "Wajid";
+
+console.log("Hello" , name)
+
+var age = 20;
+
+console.log(name, "is", age, "Years old")
+
+var anotherName = "Al";
+
